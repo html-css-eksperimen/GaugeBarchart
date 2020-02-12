5ec61de4016c65b646b00b8b29b67a6209a05da7
GulajavaMinistudio <gulajava.mini@gmail.com>
2020-02-12 09:45:21 +0700

    Init awal aplikasi

---
+++ src/app.js
@@ -0,0 +1,11 @@
+export default class CSSGaugeHelper {
+    constructor(elementId, gaugeElement, progressElement) {
+        this.elementId = elementId;
+        this.gaugeElement = gaugeElement;
+        this.progressElement = progressElement;
+    }
+
+    kalkulasiRadiusLingkuran() {}
+
+    setFillRadiusLingkaran() {}
+}
